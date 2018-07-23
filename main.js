06c2fcb4f8049cf39de2b6f05fbc5773e9a0241b
FredUA <33511388+FredUA@users.noreply.github.com>
2018-07-23 09:18:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -20,9 +20,22 @@ function Area() {
 	this._figures  = [];
 	this.addFigure = function(F) {
 		this._figures.push(F);
+		return this._figures;
 	}
 	this.clear = function() {
-		_figures = [];
+		this._figures = [];
+		return this._figures;
 	}
-	Object.defineProperty()
+	Object.defineProperty(this, "size", {
+		get: function(){
+			return res;
+		},
+		set: function(value){ 
+			var res = 0;
+			for (var key in area.addFigure){
+				res = res + key;
+			}
+		}
+	})
+	
 }
